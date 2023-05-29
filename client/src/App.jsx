be898adbba16b3b11b6e9ee9735316964be8fbb2
Tamani Phiri <tamanigabriel0@gmail.com>
2023-05-29 20:00:57 +0200

    successfull get request from client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css'
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
  
+  useEffect(()=>{
+    (async()=>{
+      const users = await axios.get('http://localhost:8080/users')
+      console.log(users);
+    })()
+  },[])
+
   return (
     <>
       <div>full stack example</div>  
